d6af4eae7b77849505eefb1cc47dedafffcb99eb
Adrian Kowalski <124436182+KowalskiAdrian@users.noreply.github.com>
2023-03-29 17:56:28 +0200

    Add files via upload

---
+++ Unit Testing Python/test_unittest.py
@@ -0,0 +1,15 @@
+import unittest   # The test framework
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(4, 4)
+
+    # This test is designed to fail for demonstration purposes.
+    def test_decrement(self):
+        self.assertEqual(4,4)
+
+    def test_decrement(self):
+        self.assertEqual(4,4)
+
+if __name__ == '__main__':
+    unittest.main
