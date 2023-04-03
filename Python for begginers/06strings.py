e308e9810176df149e0eb6838100b165aebfec5a
Adrian Kowalski <124436182+KowalskiAdrian@users.noreply.github.com>
2023-04-03 16:25:48 +0200

    Add files via upload

---
+++ Python for begginers/06strings.py
@@ -0,0 +1,9 @@
+sentence = "A sentence in here" #
+
+print(sentence)
+sentence = "A sentence in here" #
+
+print(sentence.upper())# brackets no the end. upper case letters
+
+sentence = "A sentence in here" #
+print(sentence.lower()) # brackets no the end. lower case letters
---
+++ Python for begginers/08dictionaries.py
@@ -0,0 +1,13 @@
+person = {
+    "key": "value",
+    "name": "Adrian",
+    "twitter": "@007__Adrian"
+}
+
+print(person['twitter'])
+
+print(person)
+person['instagram'] = "@niemamkonta"
+print(person)
+del person['key']
+print(person)
