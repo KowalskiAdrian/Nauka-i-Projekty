2174d84757577ee449776140d7d5cb884269e015
Adrian Kowalski <124436182+KowalskiAdrian@users.noreply.github.com>
2023-04-03 16:15:42 +0200

    Delete lekcja11 moduly.py

--- lekcja11 moduly.py
+++
@@ -1,6 +0,0 @@
-import random                           # importuj losowo liczbe
-from math import pi                     # z matematyki importuj liczbe Pi
-from math import sqrt as pierwiastek    # pierwiastek z 16
-print(random.randint(1, 10))
-print(pi)
-print(pierwiastek(16))
