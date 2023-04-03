8787386d37cb0cf0b56c70a9b67cdcaca1bb4a30
Adrian Kowalski <124436182+KowalskiAdrian@users.noreply.github.com>
2023-04-03 16:20:11 +0200

    Delete lekcja35.3 Wyrazenia regularne (cz. 4) - Regular Expression (grupy, walidacja e-mail).py

--- lekcja35.3 Wyrazenia regularne (cz. 4) - Regular Expression (grupy, walidacja e-mail).py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-if re.match(r"^([A-Za-z0-9]+|[A-Za-z0-9][A-Za-z0-9\.-]+[A-Za-z0-9])@([A-Za-z0-9]+|[A-Za-z0-9-\.]+[A-Za-z0-9])\.[A-Za-z0-9]+$", "b.a@p.pl"):
-    print("Dopasowano!")                # dopasowano email
-else:
-    print("Nie dopasowano!")                          
