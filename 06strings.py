09b2293c406f3b958cfc6310bea22b534573514a
Adrian Kowalski <124436182+KowalskiAdrian@users.noreply.github.com>
2023-04-03 16:26:54 +0200

    Delete 06strings.py

--- 06strings.py
+++
@@ -1,9 +0,0 @@
-sentence = "A sentence in here" #
-
-print(sentence)
-sentence = "A sentence in here" #
-
-print(sentence.upper())# brackets no the end. upper case letters
-
-sentence = "A sentence in here" #
-print(sentence.lower()) # brackets no the end. lower case letters
